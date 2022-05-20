0937bc0d5dc793e97b0dde959a26b2a1dec48f47
Nikita <kabanov.n.v@mail.ru>
2022-05-20 20:26:23 +0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,8 @@ function App() {
     const ref = useRef(null);
     console.log(todos);
     return (
-        <div className='container'>
-            <h1 className='todo__title'>ToDo List:</h1>
-            <ul className='todo__list'>
-                {todos.map((todo) => {
-                    return (
-                        <li className='todo__item' key={todo.id}>
-                            <span
-                                style={
-                                    todo.completed
-                                        ? { textDecoration: 'line-through' }
-                                        : null
-                                }
-                                onClick={() => dispatch(toggleTodo(todo.id))}>
-                                {todo.title}
-                            </span>
-                            <button
-                                onClick={() => dispatch(removeTodo(todo.id))}>
-                                x
-                            </button>
-                        </li>
-                    );
-                })}
-            </ul>
+        <div className='container orange lighten-5 z-depth-3'>
+            <h1 className='todo__title'>Список дел</h1>
             <form
                 action='submit'
                 onSubmit={(evt) => {
@@ -38,9 +17,44 @@ function App() {
                     dispatch(addTodo(ref.current.value));
                     ref.current.value = '';
                 }}>
-                <input ref={ref} type='text' placeholder='new ToDo' />
-                <button>Add ToDo</button>
+                <input
+                    id='input_text'
+                    ref={ref}
+                    type='text'
+                    placeholder='Например, новая задача'
+                />
+                <button className='btn waves-light'>Добавить</button>
             </form>
+            <ul className='todo__list'>
+                {todos.map((todo) => {
+                    return (
+                        <li className='todo__item' key={todo.id} value={1}>
+                            <label>
+                                <input
+                                    className='filled-in'
+                                    type='checkbox'
+                                    onClick={() =>
+                                        dispatch(toggleTodo(todo.id))
+                                    }
+                                />
+                                <span
+                                    style={
+                                        todo.completed
+                                            ? { textDecoration: 'line-through' }
+                                            : null
+                                    }>
+                                    {todo.title}
+                                </span>
+                            </label>
+                            <i
+                                class='material-icons secondary-content'
+                                onClick={() => dispatch(removeTodo(todo.id))}>
+                                delete
+                            </i>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
